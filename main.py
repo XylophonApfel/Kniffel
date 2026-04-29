6562a7176142252854b60184f550988e5e8b8817
Aron <lanzendoerfer.aron@gmail.com>
2026-04-29 12:48:01 +0200

    Window main eingebaut

--- main.py
+++ main.py
@@ -8,3 +8,29 @@ import rules
 import scorecard
 
 os.system("cls")
+
+
+
+
+def main():
+    pygame.init()
+    window = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption("Kniffel")
+    clock = pygame.time.Clock()
+
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        window.fill((52, 235, 55))
+
+        pygame.display.flip()
+        clock.tick(60)
+
+
+
+if __name__ == "__main__":
+    main()
